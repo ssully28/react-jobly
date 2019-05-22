7bf279b453f96477854af739810c031efbf9d4d4
Steve Sullivan <ssully28@gmail.com>
2019-05-22 10:35:13 -0700

    starting jobly project

---
+++ jobly/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h2>App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
