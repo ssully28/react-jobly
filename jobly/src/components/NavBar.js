9993c3a468fe0028a45e8216eecc5ce6a6e941ad
Jason Matthias <jpmatthias90@gmail.com>
2019-05-24 09:02:23 -0700

    navbar woes

--- jobly/src/components/NavBar.js
+++ jobly/src/components/NavBar.js
@@ -1,41 +1,74 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import './NavBar.css';
 import { getCurrentUser } from '../helpers';
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.logUserOut = this.logUserOut.bind(this);
-  }
-  
-  logUserOut() {
-    window.localStorage.removeItem('joblyUser');
-    this.forceUpdate();
-    this.props.history.push("/");
-  }
+		this.logUserOut = this.logUserOut.bind(this);
+		this.loggedInNav = this.loggedInNav.bind(this);
+		this.loggedOutNav = this.loggedOutNav.bind(this);
+	}
 
-  render() {
+	logUserOut() {
+		window.localStorage.removeItem('joblyUser');
+		this.forceUpdate();
+		this.props.history.push('/');
+	}
 
-    const user = getCurrentUser();
+	loggedInNav() {
+		return (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item mr-4">
+					<NavLink className="nav-link" exact to="/companies" activeClassName="active">
+						Companies
+					</NavLink>
+				</li>
+				<li className="nav-item mr-4">
+					<NavLink className="nav-link" exact to="/jobs" activeClassName="active">
+						Jobs
+					</NavLink>
+				</li>
+        <li className="nav-item mr-4">
+            <Link className="nav-link" exact to="/" onClick={this.logUserOut} activeClassName="active">
+              Log Out
+            </Link>
+        </li>
+			</ul>
+		);
+	}
 
-    const logInOrOut = user
-      ? <button className="btn btn-link" onClick = {this.logUserOut}>Logout</button>
-      : <NavLink className="mx-2" exact to="/login" activeClassName="active">Login</NavLink>
-    
-    return (
-      <div className="row d-flex py-3 mb-5 align-items-center nav-div" >
-        <h3 className="col-2" >  <NavLink exact to="/">Jobly</NavLink>  </h3>
-        <nav className="nav-bar-custom col-10 pr-5 d-flex justify-content-end align-items-center" >
-          <NavLink className="mx-2" exact to="/companies" activeClassName="active">Companies</NavLink>
-          <NavLink className="mx-2" exact to="/jobs" activeClassName="active">Jobs</NavLink>
-          { logInOrOut }
-        </nav>
-      </div>
-    );
-  }
+	loggedOutNav() {
+		return (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item mr-4">
+					<Link className="nav-link" exact to="/Login">
+						Log In
+					</Link>
+				</li>
+			</ul>
+		);
+	}
+
+	render() {
+		const user = getCurrentUser();
+
+		const logInOrOut = user ? this.loggedInNav() : this.loggedOutNav();
+
+		return (
+			
+				<nav className="nav-bar-custom navbar navbar-expand-md">
+          <Link className="navbar-brand" to="/">
+            Jobly
+          </Link>
+					{logInOrOut}
+				</nav>
+			
+		);
+	}
 }
 
 export default withRouter(NavBar);
